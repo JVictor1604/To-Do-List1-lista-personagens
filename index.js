a7b63b4b0de0da2665097d8e8dcf1c1eee1c5254
JVictor1604 <josevictor8569@gmail.com>
2022-04-19 20:31:53 -0300

    commit 19/04

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const port = 3000;
 const app = express();
 const cors = require('cors');
 const route = require('./src/routes/jojo.routes');
-
+const connectToDatabase = require('./src/database/database');
 
 
 
 app.use(express.json());
 app.use(cors());
+
+connectToDatabase();
 
 app.use('/', route);
 
