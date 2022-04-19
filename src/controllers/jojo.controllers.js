78485f3b151f5d5deada9bedf8db567fd46d8812
JVictor1604 <josevictor8569@gmail.com>
2022-04-18 21:50:41 -0300

    commit 18/04

--- src/controllers/jojo.controllers.js
+++ src/controllers/jojo.controllers.js
@@ -9,38 +9,31 @@ const findJojoById = (req, res) => {
   res.send(JojosService.findJojoByIdService(req.params.id));
 };
 
-const addJojo = (req,res) => {
+const addJojo = (req, res) => {
+  let retorno = JojosService.addJojoService(req.body);
 
-    
-    let retorno =  JojosService.addJojoService(req.body) ;
-
-    if(retorno == "ok") {
-
-    res.send("Personagem cadastrado com sucesso")} else {res.send('Houve um erro')}
-
+  if (retorno == 'ok') {
+    res.send('Personagem cadastrado com sucesso');
+  } else {
+    res.send('Houve um erro');
+  }
 };
 
 const updateJojosController = (req, res) => {
+  const updatedJojo = JojosService.uptadeJojoService(+req.params.id, req.body);
+  res.send(updatedJojo);
+};
 
-    
-    const updatedJojo = JojosService.uptadeJojoService(+req.params.id, req.body);
-    res.send(updatedJojo);
+const deleteJojo = (req, res) => {
+  JojosService.deleteJojoService(req.params.id);
 
-  };
-
-const deleteJojo = (req,res) => {
-
-
-    JojosService.deleteJojoService(req.params.id)
-
-    res.send("Personagem deletado com sucesso")
-
+  res.send('Personagem deletado com sucesso');
 };
 
 module.exports = {
-    findJojo,
-    findJojoById,
-    addJojo,
-    deleteJojo,
-    updateJojosController
+  findJojo,
+  findJojoById,
+  addJojo,
+  deleteJojo,
+  updateJojosController,
 };
